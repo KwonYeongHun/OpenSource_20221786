90d50b4e2a6a0f88dc41f3ac792b2c68c586b542
KwonYeongHun <yeonghun@soongsil.ac.kr>
2022-11-02 10:37:01 +0900

    opensource#3

--- src/ex3-6.sh
+++ src/ex3-6.sh
@@ -15,9 +15,9 @@ touch file3.txt
 touch file4.txt
 
 tar -cvf files.tar file0.txt file1.txt file2.txt file3.txt file4.txt
-mkdir files
-mv files.tar files
-cd files
+mkdir $i
+mv files.tar $i
+cd $i
 tar -xvf files.tar
 
 exit 0
